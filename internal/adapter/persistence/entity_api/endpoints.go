fdce98b1af6f73a369fc791f9fc1e9ae81b78edf
programanda03 <paganiamanda791@gmail.com>
2026-02-03 02:07:48 -0300

    fix warning and nil pointer error

--- internal/adapter/persistence/entity_api/endpoints.go
+++ internal/adapter/persistence/entity_api/endpoints.go
@@ -6,13 +6,13 @@ const (
 
 const (
 	VEHICLES_ENDPOINT = BASE_URL + "/vehicles"
-	VEHICLES_ID_ENDPOINT = BASE_URL + "/vehicles/%s"
+	VEHICLES_ID_ENDPOINT = BASE_URL + "/vehicles/%d"
 	VEHICLES_PLATE_ENDPOINT = BASE_URL + "/vehicles/plate/%s"
-	VEHICLES_CUSTOMER_ID_ENDPOINT = BASE_URL + "/vehicles/customer/%s"
+	VEHICLES_CUSTOMER_ID_ENDPOINT = BASE_URL + "/vehicles/customer/%d"
 )
 
 const (
 	CUSTOMERS_ENDPOINT = BASE_URL + "/customers"
-	CUSTOMERS_ID_ENDPOINT = BASE_URL + "/customers/%s"
+	CUSTOMERS_ID_ENDPOINT = BASE_URL + "/customers/%d"
 	CUSTOMERS_DOCUMENT_ENDPOINT = BASE_URL + "/customers/document/%s"
 )
